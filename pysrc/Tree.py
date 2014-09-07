6ab9e8b4d7c2f85412437ac146dfdea548f9863a
Ian Ooi <ionaic@gmail.com>
2014-09-06 20:13:06 -0700

    attempting to fix the tree issue

--- pysrc/Tree.py
+++ pysrc/Tree.py
@@ -6,20 +6,35 @@ g = vtk.vtkMutableDirectedGraph()
 
 # Create 10 vertices
 verts = [g.AddVertex() for i in range(0, 10)]
+#v1 = g.AddVertex()
+#v2 = g.AddVertex()
+#
+#g.AddGraphEdge(v1, v2)
 
 # Create a tree
-g.AddGraphEdge(verts[4], verts[8])
-g.AddGraphEdge(verts[6], verts[8])
-g.AddGraphEdge(verts[0], verts[6])
-g.AddGraphEdge(verts[2], verts[6])
-g.AddGraphEdge(verts[1], verts[3])
-g.AddGraphEdge(verts[3], verts[5])
-g.AddGraphEdge(verts[5], verts[9])
-g.AddGraphEdge(verts[7], verts[9])
-g.AddGraphEdge(verts[8], verts[9])
+g.AddGraphEdge(verts[1], verts[0])
+g.AddGraphEdge(verts[3], verts[1])
+g.AddGraphEdge(verts[5], verts[1])
+g.AddGraphEdge(verts[7], verts[5])
+g.AddGraphEdge(verts[9], verts[5])
+g.AddGraphEdge(verts[2], verts[0])
+g.AddGraphEdge(verts[4], verts[2])
+g.AddGraphEdge(verts[6], verts[2])
+g.AddGraphEdge(verts[8], verts[4])
+#g.AddGraphEdge(verts[4], verts[8])
+#g.AddGraphEdge(verts[6], verts[8])
+#g.AddGraphEdge(verts[0], verts[6])
+#g.AddGraphEdge(verts[2], verts[6])
+#g.AddGraphEdge(verts[1], verts[3])
+#g.AddGraphEdge(verts[3], verts[5])
+#g.AddGraphEdge(verts[5], verts[9])
+#g.AddGraphEdge(verts[7], verts[9])
+#g.AddGraphEdge(verts[8], verts[9])
 
 tree = vtk.vtkTree()
-tree.CheckedShallowCopy(g)
+success = tree.CheckedShallowCopy(g)
+
+print("Success? " + str(success))
 
 graphLayoutView = vtk.vtkGraphLayoutView()
 graphLayoutView.AddRepresentationFromInput(tree)
